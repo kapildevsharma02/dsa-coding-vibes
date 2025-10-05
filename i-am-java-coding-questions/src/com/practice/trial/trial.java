da85485ee20708ffbab8f5c4abcea14d039ef57b
2270366 <2270366@LTIN511242.cts.com>
2025-10-05 11:22:00 +0530

    checking the github connect....

---
+++ i-am-java-coding-questions/src/com/practice/trial/trial.java
@@ -0,0 +1,10 @@
+package com.practice.trial;
+
+public class trial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Checking the github connect....");
+	}
+
+}
